d7c6a2fc5bbcb965cce47b4dd6ebd10e732098a8
ehsan fathi <ehsanf_89180@live.com>
2026-06-20 17:11:41 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Product> products = new List<Product>();
+
+        products.Add(new Product { Id = 1, Name = "dicast", Price = 150000 });
+        products.Add(new Product { Id = 2, Name = "PU", Price = 20000 });
+        products.Add(new Product { Id = 3, Name = "chodani", Price = 30000 });
+
+
+        var expensiveprods = products.Where(p => p.Price > 30000);
+
+        foreach (var pros in expensiveprods)
+        {
+            Console.WriteLine($"Id:{pros.Id},Name:{pros.Name},Price:{pros.Price}");
+        }
+    }
+
+}
