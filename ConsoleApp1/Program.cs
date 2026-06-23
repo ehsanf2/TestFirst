fa99c295a695d94a86f0040a46ecd2213ff4c242
ehsan fathi <ehsanf_89180@live.com>
2026-06-23 08:53:50 +0330

    Add Delete Method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,6 +15,8 @@ class Program
         Console.WriteLine("2-  Add product");
         Console.WriteLine("3- search name");
         Console.WriteLine("4- ordeby Price");
+        Console.WriteLine("5- Get By ID");
+        Console.WriteLine("6- Delete Product");
 
         string menuselcstion = Console.ReadLine();
         int menusel = 0;
@@ -55,6 +57,39 @@ class Program
                     Console.WriteLine($"Id = {item.Id} \n Name = {item.Name} \n Price = {item.Price}");
                 }
                 break;
+
+            case "5":
+                Console.WriteLine("Enter Id:");
+                int id = Convert.ToInt32(Console.ReadLine());
+                var searchedProduct = proManger.GetById(id);
+                if (searchedProduct != null)
+                {
+                    Console.WriteLine($"Id = {searchedProduct.Id} \n Name = {searchedProduct.Name} \n Price = {searchedProduct.Price}");
+                }
+                else
+                {
+                    Console.WriteLine($"This Id is not created");
+
+                }
+                break;
+            case "6":
+                Console.WriteLine("Enter Product ID:");
+
+                 id = Convert.ToInt32(Console.ReadLine());
+                var selDeleteProduct = proManger.GetById(id);
+                if (selDeleteProduct != null)
+                {
+                    bool delResult = proManger.DeleteProduct(selDeleteProduct.Id);
+                    if (delResult)
+                        Console.WriteLine("Product deleted successfull!!");
+                    else
+                        Console.WriteLine("Product deleted has an error!!");
+
+                }
+                else
+                    Console.WriteLine("Product Not found!");
+                break;
+
         
         default:
                 break;
---
+++ ConsoleApp1/Services/ProductMangement.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+
+public class ProductManager
+{
+    private string filePath = "Products.json";
+    private List<Product> Products = new();
+
+
+    public ProductManager()
+    {
+        LoadProducts();
+    }
+
+    private void LoadProducts()
+    {
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            var readPros = JsonSerializer.Deserialize<List<Product>>(json);
+            if (readPros != null)
+                Products = readPros;
+        }
+    }
+
+    private void SaveProducts()
+    {
+        string json =
+            JsonSerializer.Serialize(Products,
+            new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+        File.WriteAllText(filePath, json);
+    }
+
+    public bool DeleteProduct(int Id)
+    {
+        var product = Products.FirstOrDefault(p => p.Id == Id);
+
+        if (product == null)
+            return false;
+
+        else
+        {
+            Products.Remove(product);
+
+            SaveProducts();
+            return true;
+        }
+    }
+    public List<Product> GetAllProducts()
+    {
+        return Products;
+    }
+
+    public void AddProduct(Product product)
+    {
+        product.Id = Products.Count + 1;
+        Products.Add(product);
+        SaveProducts();
+    }
+
+    public List<Product> SearchAll(string name)
+    {
+        return Products.Where(p => p.Name.Contains(name)).ToList();
+
+    }
+
+    public List<Product> OrderbyPrice()
+    {
+        return Products.OrderBy(p => p.Price).ToList();
+    }
+
+
+    public Product? GetById(int id)
+    {
+        return Products.FirstOrDefault(p => p.Id == id);
+    }
+}
